53944b3ab2d1eaea8c1c9cf8947e6a337c5f60a1
taroun <aksghk4671@gmail.com>
2021-07-08 16:00:40 +0900

    7/8 new 1ban commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,11 +5,20 @@ from django.shortcuts import render
 
 #def hello_world(request):
 #    return HttpResponse("안녕하세요!")
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == 'POST':
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, "accountapp/hello_world.html",
-                      context={'text':'POST METHOD'})
+                      context={'hello_world_output':new_hello_world})
     else:
         return render(request, "accountapp/hello_world.html",
                       context={'text':'GET METHOD'})
