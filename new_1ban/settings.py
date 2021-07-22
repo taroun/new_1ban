ff2328ef4c10b635f3abbd2f0aa1042b4014abae
taroun <aksghk4671@gmail.com>
2021-07-22 14:57:19 +0900

    7/22 new 1ban commit 1

--- new_1ban/settings.py
+++ new_1ban/settings.py
@@ -143,9 +143,15 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
