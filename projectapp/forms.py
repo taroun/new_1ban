9267f8f4772e7dd23f3fff09f6dee78adeb85309
taroun <aksghk4671@gmail.com>
2021-08-12 16:16:29 +0900

    8/12 new 1ban commit 7

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -6,4 +6,4 @@ from projectapp.models import Project
 class ProjectCreationForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['name', 'description', 'image']
+        fields = ['name', 'image', 'description']
