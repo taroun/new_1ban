d60df1eb8e73df6fe2328d2b6f713316e2d25ae7
taroun <aksghk4671@gmail.com>
2021-08-25 10:33:03 +0900

    8/25 new 1ban commit 4

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -11,25 +13,39 @@ from django.views.generic import RedirectView
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
+@transaction.atomic
+def db_transaction(user, article, likeRecord):
+
+    if likeRecord.exists():
+
+        raise ValidationError('좋아요가 이미 존재합니다.')
+
+    else:
+        LikeRecord(user=user, article=article).save()
+
+    article.like += 1
+    article.save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-
         likeRecord = LikeRecord.objects.filter(user=user,
                                                article=article)
 
-        if likeRecord.exists():
-            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
+        try:
+            db_transaction(user, article, likeRecord)
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영 되었습니다.')
+        except ValidationError:
+            likeRecord.delete()
+            article.like -= 1
+            article.save()
+            messages.add_message(request, messages.SUCCESS, '좋아요가 취소되었습니다.')
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
 
-        article.like += 1
-        article.save()
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영 되었습니다.')
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
