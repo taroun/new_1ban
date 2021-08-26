3afdbdd550a6b76bc23e382583a761955181ffee
taroun <aksghk4671@gmail.com>
2021-08-26 16:06:52 +0900

    8/26 new 1ban commit 4

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,16 +2,18 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from articleapp.views import ArticleListView
 
 app_name = 'accountapp'
 
 urlpatterns = [
+    path('', ArticleListView.as_view(), name='home'),
 
-    path('login/', LoginView.as_view(template_name='accountapp/login.html'),name='login'),
-    path('logout/',LogoutView.as_view(),name='logout'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
-    path('create/',AccountCreateView.as_view(),name='create'),
-    path('detail/<int:pk>',AccountDetailView.as_view(),name='detail'),
-    path('update/<int:pk>',AccountUpdateView.as_view(),name='update'),
-    path('delete/<int:pk>',AccountDeleteView.as_view(),name='delete'),
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
