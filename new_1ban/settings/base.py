acd8ee695efae43dd5ed9f81a461e125296e9c03
taroun <aksghk4671@gmail.com>
2021-09-09 16:09:31 +0900

    9/9 new 1ban commit 1

--- new_1ban/settings/base.py
+++ new_1ban/settings/base.py
@@ -16,33 +16,15 @@ from pathlib import Path
 from django.urls import reverse_lazy
 from django.utils import timezone
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-env_list = dict()
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find("=")
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -102,15 +84,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'new_1ban.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
